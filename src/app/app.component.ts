b0a6407f405f9d5c7fe46be0998b64702806ca78
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2023-05-01 12:12:40 +0600

    assignment 3 have been done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles:  [`
-    
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   username = '';
+  showSecret=false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
